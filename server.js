00d42387c24c30775ff824ba589b23a4317a3772
felipedelatorre <felipede_latorre@hotmail.com>
2019-04-30 09:27:39 -0700

    server listening

--- server.js
+++ server.js
@@ -12,3 +12,29 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 
+//API Routes
+app.get('/location', (request, response) => {
+  try {
+    const locationData = searchCoords(request.query.data);
+    response.send(locationData);
+  }
+  catch(error) {
+    console.error(error);
+    response.status(500).send('Status: 500. You suck. User error');
+  }
+});
+
+//Helper Function
+let searchCoords = (query) => {
+  const geoDate = require('./data/geo.json');
+  const location = {
+    search_query: query,
+    formatted_query: geoData.results[0].formatted_address,
+    latitude: geoData.results[0].geometry.location.lat,
+    longitude: geoData.results[0].geometry.location.lng
+
+  };
+  return location;
+}
+
+app.listen(PORT, () => console.log(`app is listening ${PORT}`));
